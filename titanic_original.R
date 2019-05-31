9e838d52d9cbacf507e63a120ffab09a2ea658f1
ARhodes30 <ashleyrhds@gmail.com>
2019-05-31 08:44:42 -0500

    Add files via upload

---
+++ titanic_original.R
@@ -0,0 +1,49 @@
+library(dplyr)
+library(tidyr)
+library(readxl)
+library(stringr)
+
+titanic_original <- read.csv("~/R projects/Titanic/titanic3.csv")
+View(titanic_original)
+
+
+#see how many missing values exist. It looks like there are three.
+summary(titanic_original$embarked)
+
+#replace missing values with "s" 
+titanic_original$embarked[titanic_original$embarked == ""] <- "S"
+
+
+#Calculate the mean of the Age column and use that value to populate the missing values
+hist(titanic_original$age)
+
+titanic_original %>% 
+  summarise(avg_age = mean(age, na.rm = TRUE))
+
+titanic_original <- titanic_original %>% 
+  replace_na(list(age = 30))
+
+summary(titanic_original$age)
+
+
+
+# find missing values in cabin,then place 0 for empty spaces or 1 if cabin column has value
+titanic_original <- titanic_original %>%
+  mutate(has_cabin_number = ifelse(cabin == "", 0, 1))
+
+
+
+#Fill these empty slots with a dummy value e.g. the string 'None' or 'NA'
+titanic_original$boat[titanic_original$boat == ""] <- "NA"
+
+
+
+
+
+
+View(titanic_original)
+  
+
+
+
+  
